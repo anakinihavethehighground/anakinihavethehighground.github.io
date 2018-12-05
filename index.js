840316ed27b447e40a1356f9d8d892546754e8cb
duckliningdon <duckliningdon@gmail.com>
2018-12-05 17:42:43 -0600

    successful nav

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1075 || document.documentElement.scrollTop > 1075) {
+  if (document.body.scrollTop > 1060 || document.documentElement.scrollTop > 1060) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
